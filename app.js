4a29b8a4d8cc91de32cc43b4aa4bf7d851e90722
NCV-DJ <ncvirtusa.dj@gmail.com>
2016-02-29 18:29:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 console.log('server starting');
 
 app.get('/', function(req, res){
-    res.send('Hello from inside a container!');
+    res.send('Hello  from inside a container!');
 });
 
 app.listen(8080);
